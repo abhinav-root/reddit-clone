cd69a286bc478d9d2c203dcebea82237370b9c54
Abhinav Singh <abhinavsinghkkr@gmail.com>
2024-05-08 18:43:51 +0530

    Remove next-auth

--- src/app/login/_actions/index.ts
+++ src/app/login/_actions/index.ts
@@ -2,31 +2,12 @@
 
 import { isRedirectError } from "next/dist/client/components/redirect";
 
-import { signIn } from "@/auth";
 import { loginSchema, LoginSchema } from "../_schemas";
-import { AuthError } from "next-auth";
 
 export async function login(form: LoginSchema) {
   const validationResult = loginSchema.safeParse(form);
   if (!validationResult.success) {
     return { success: false, error: "Validation error" };
   }
-  const formData = new FormData();
-  formData.append("email", form.email);
-  formData.append("password", form.password);
-  try {
-    await signIn("credentials", formData);
-  } catch (error) {
-    console.log(error);
-    if (isRedirectError(error)) {
-      throw error;
-    }
-
-    if (error instanceof AuthError) {
-      if (error.type === "CredentialsSignin") {
-        return { success: false, error: "Invalid email or password" };
-      }
-    }
-    return { success: false, error: "Internal Server error" };
-  }
+  const { email, password } = validationResult.data;
 }
--- src/app/signup/_actions/index.ts
+++ src/app/signup/_actions/index.ts
@@ -2,14 +2,11 @@
 
 import * as crypto from "crypto";
 
-import { signIn } from "@/auth";
 import { SignupSchema, signupSchema } from "../_schemas";
 import prisma from "@/helpers/db";
-import { redirect } from "next/navigation";
-import { AuthError } from "next-auth";
 
 export async function loginWithGoogle() {
-  await signIn("google");
+  // await signIn("google");
 }
 
 export async function signup(form: SignupSchema) {
@@ -33,25 +30,14 @@ export async function signup(form: SignupSchema) {
       };
     }
     const salt = generateSalt(100);
-    const bcrypt = require("bcrypt");
-    const hashedPassword = await bcrypt.hash(password + salt + pepper, 12);
+    // const hashedPassword = await bcrypt.hash(password + salt + pepper, 12);
+    const hashedPassword = ""
     await prisma.user.create({
       data: { username, email, password: hashedPassword, salt },
     });
-    const formData = new FormData();
-    formData.append("email", email);
-    formData.append("password", password);
-    await signIn("credentials", {email, password, redirect: false});
     return { success: true, message: "Account created successfully." };
   } catch (err) {
-    // TODO: AuthError handling
     console.log(err);
-    if (err instanceof AuthError) {
-      if (err.type === "CallbackRouteError") {
-        return { success: false, error: "Invalid email or password" };
-      }
-    }
-    return { success: false, error: "Internal Server Error." };
   }
 }
 
